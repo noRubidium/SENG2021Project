d7376f9b266d8236b0658aa7541e977a840607cf
norubidium <markshen5295@hotmail.com>
2016-09-15 17:47:42 +1000

    add compile script

--- src/js/actions/videoSearchActions.js
+++ src/js/actions/videoSearchActions.js
@@ -3,8 +3,9 @@ import axios from "axios";
 export function fetchVideos(searchTerm) {
   const url = "https://www.googleapis.com/youtube/v3/search?maxResults=9&hl=en&part=snippet&type=video&key=AIzaSyDmsTbEA0uhRwaFmxfCM8w_jyaDoFdWM1o&q="
   //https://www.googleapis.com/youtube/v3/search?part=snippet&q=python&type=video&key=AIzaSyDmsTbEA0uhRwaFmxfCM8w_jyaDoFdWM1o
+  searchTerm = searchTerm.replace(" ","%20")+"%20tutorial"
   return function(dispatch) {
-    axios.get(url+searchTerm+"%20tutorial")
+    axios.get(url+searchTerm)
       .then((response) => {
         dispatch({type: "FETCH_VIDEOS_FULFILLED", payload: response.data})
       })
