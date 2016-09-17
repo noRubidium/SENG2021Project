cb8ec2b0b3fb7ee014a6dbcb3f8b5b518956862e
leon-ng9 <leon.ng9@outlook.com>
2016-09-18 03:30:50 +1000

    Change footer

--- src/js/components/layout/Footer.js
+++ src/js/components/layout/Footer.js
@@ -3,14 +3,19 @@ import React from "react";
 
 export default class Footer extends React.Component {
   render() {
+    const footerStyle = {
+      right: "0",
+      bottom: "0",
+      left: "0",
+      padding: "1rem",
+      backgroundColor: "#efefef",
+      textAlign: "center",
+    }
+
     return (
-      <footer>
-        <div class="footer navbar-default navbar-fixed-bottom">
-          <div class="col-md-12">
-            &#47;&#47;TODO: Team Name
-          </div>
-        </div>
-      </footer>
+      <div style={footerStyle}>
+        '&#47;&#47;TODO: Team Name' (Hussein, Jessica, Leon, Mazen, Minjie)
+      </div>
     );
   }
 }
