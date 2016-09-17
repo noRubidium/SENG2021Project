a00817cb4bf2a4e99ac3b424a91d45b956f1d3b5
leon-ng9 <leon.ng9@outlook.com>
2016-09-17 22:09:58 +1000

    Push for jessica to do

--- src/js/client.js
+++ src/js/client.js
@@ -6,7 +6,7 @@ import { Router, Route, IndexRoute, hashHistory } from "react-router";
 import Layout from "./pages/Layout"
 import AllSearch from "./components/AllSearch"
 import Dashboard from "./components/Dashboard"
-import Forum from "./components/Forum"
+import ForumSearch from "./components/ForumSearch"
 import Video from "./components/Video"
 import Tweet from "./components/Twitter"
 import GithubSearch from "./components/GithubSearch"
@@ -23,7 +23,7 @@ ReactDOM.render(<Provider store={store}>
       <IndexRoute component={Search}></IndexRoute>
       <Route path="all(/:search)" component={AllSearch}></Route>
       <Route path="dashboard" component={Dashboard}></Route>
-      <Route path="forum(/:search)" component={Forum}></Route>
+      <Route path="forum(/:search)" component={ForumSearch}></Route>
       <Route path="github(/:search)" component={GithubSearch}></Route>
       <Route path="github/display/:owner/:repo" component={Github}></Route>
       <Route path="video/:term" component={VideoSearch}></Route>
