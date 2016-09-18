dc7d39faa697c33ad6d95a0a661f5e6929331550
norubidium <markshen5295@hotmail.com>
2016-09-18 22:26:49 +1000

    Fix word not wrapping taking up too much space

--- src/js/actions/videoSearchActions.js
+++ src/js/actions/videoSearchActions.js
@@ -4,7 +4,7 @@ export function fetchVideos(searchTerm, pageToken=null) {
   const page= pageToken? ("&pageToken=" + pageToken) : ""
   const url = "https://www.googleapis.com/youtube/v3/search?maxResults=9&hl=en&part=snippet&type=video&key=AIzaSyDmsTbEA0uhRwaFmxfCM8w_jyaDoFdWM1o&q="
   //https://www.googleapis.com/youtube/v3/search?part=snippet&q=python&type=video&key=AIzaSyDmsTbEA0uhRwaFmxfCM8w_jyaDoFdWM1o
-  searchTerm = searchTerm.replace(" ","%20")+"%20tutorial"
+  searchTerm = searchTerm.replace(/\s*$/,"").replace(" ","%20")+"+tutorial"
   return function(dispatch) {
     axios.get(url+searchTerm + page)
       .then((response) => {
