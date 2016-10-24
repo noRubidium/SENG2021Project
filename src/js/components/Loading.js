5ce9457600d46b0553abeb8ff397dfc5621e098e
norubidium <markshen5295@hotmail.com>
2016-10-24 12:15:28 +1100

    image indexing fix

--- src/js/components/Loading.js
+++ src/js/components/Loading.js
@@ -4,7 +4,7 @@ export default class Loading extends React.Component {
   render() {
     return (
       <div>
-          <center><img src="../../loading.gif" style={{verticalAlign: "top", width: "120px", marginTop: "50px", marginBottom: "10px"}}/></center>
+          <center><img src="loading.gif" style={{verticalAlign: "top", width: "120px", marginTop: "50px", marginBottom: "10px"}}/></center>
           <div class="row">
             <center><h3 class="loading" style={{color: "#446CB3"}}><strong>Squeezing our sauces</strong></h3></center>
           </div>
